01873587c011e1203048026ce2a39f62dfa319af
AztekTR <rubickaghanims@gmail.com>
2021-07-05 17:45:44 +0300

    change css to scss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import { ProductList } from "./components/ProductList/ProductList";
 import { Cart } from "./components/Cart/Cart";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
